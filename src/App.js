43b93ffac2694d3f5517dc28782fbe1a6368a9d1
Adhney <adhney.nawghare@gmail.com>
2021-03-31 10:37:17 +0530

    removed unnecessary parts

--- src/App.js
+++ src/App.js
@@ -4,27 +4,18 @@ import Home from "./Components/Home";
 
 import './App.css'
 import EditCustomer from "./Components/EditCustomer";
-import Fragment from "./Components/fragment";
-
-import Hero from './Day10/Hero'
 
 function App() {
     return(
         <>
-            <Hero hero={'superman'} />
-            <Hero hero={'batman'} />
-            <ErrorBoundary>
 
-            </ErrorBoundary>
-            <Hero hero={'joker'} />
-            {/*<Router>*/}
-            {/*    <Switch>*/}
-            {/*        <Route path={'/'} exact component={Home} />*/}
-            {/*        <Route path={'/addCustomer'} exact component={AddCustomer} />*/}
-            {/*        <Route path={'/editCustomer/:id'} exact component={EditCustomer} />*/}
-            {/*        <Route path={'/fragment'} exact component={Fragment} />*/}
-            {/*    </Switch>*/}
-            {/*</Router>*/}
+            <Router>
+                <Switch>
+                    <Route path={'/'} exact component={Home} />
+                    <Route path={'/addCustomer'} exact component={AddCustomer} />
+                    <Route path={'/editCustomer/:id'} exact component={EditCustomer} />
+                </Switch>
+            </Router>
         </>
     )
 }
