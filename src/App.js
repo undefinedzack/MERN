7327a10178a916d9583ac3fb3e4610174a7883aa
Adhney <adhney.nawghare@gmail.com>
2021-08-05 09:45:42 +0530

    August 5 2021

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import {BrowserRouter as Router, Link, Switch, Route} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import AddCustomer from "./Components/AddCustomer";
 import Home from "./Components/Home";
 
-import './App.css'
+import "./App.css";
 import EditCustomer from "./Components/EditCustomer";
+import Cake from "./Components/Cake";
 
 function App() {
-    return(
-        <>
-
-            <Router>
-                <Switch>
-                    <Route path={'/'} exact component={Home} />
-                    <Route path={'/addCustomer'} exact component={AddCustomer} />
-                    <Route path={'/editCustomer/:id'} exact component={EditCustomer} />
-                </Switch>
-            </Router>
-        </>
-    )
+  return (
+    <>
+      <Router>
+        <Switch>
+          <Route path={"/"} exact component={Cake} />
+          {/* <Route path={"/"} exact component={Home} /> */}
+          <Route path={"/addCustomer"} exact component={AddCustomer} />
+          <Route path={"/editCustomer/:id"} exact component={EditCustomer} />
+        </Switch>
+      </Router>
+    </>
+  );
 }
 
 export default App;
---
+++ src/Components/Cake.js
@@ -0,0 +1,7 @@
+import { buyCake } from "../redux";
+
+const Cake = () => {
+  return <>No. of Cakes =</>;
+};
+
+export default Cake;
